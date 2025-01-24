748726aacec488edae8f78238d2addbd9ca56c3c
krianlloydl <krianlloydl@gmail.com>
2025-01-24 11:30:46 +0800

    Fix toggle navbar for mobile devices

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import UserContext from "../context/UserContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const Navbar = () => {
   const { user } = useContext(UserContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav class="bg-white border-b border-gray-300">
@@ -29,7 +34,8 @@ const Navbar = () => {
             type="button"
             class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-800 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
             aria-controls="navbar-cta"
-            aria-expanded="false"
+            aria-expanded={isMenuOpen}
+            onClick={toggleMenu}
           >
             <span class="sr-only">Open main menu</span>
             <svg
@@ -50,7 +56,9 @@ const Navbar = () => {
           </button>
         </div>
         <div
-          class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
           id="navbar-cta"
         >
           <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
@@ -125,7 +133,6 @@ const Navbar = () => {
                 <li>
                   <Link
                     to="/login"
-                    href="#"
                     class="block py-2 px-3 md:p-0 text-gray-800
                 00 rounded hover:text-teal-600"
                   >
@@ -133,7 +140,11 @@ const Navbar = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link to="/register" class="hover:text-teal-600">
+                  <Link
+                    to="/register"
+                    class="block py-2 px-3 md:p-0 text-gray-800
+                00 rounded hover:text-teal-600"
+                  >
                     Register
                   </Link>
                 </li>
